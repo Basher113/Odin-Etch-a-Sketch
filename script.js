f16ef6236cb716182a81a8504f42f41c0de747a4
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-23 09:04:59 +0800

    Add feature allowing user to set grid size
    - Prompt user for grid size input
    - Fill boxes with black on hover

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const container = document.querySelector(".container");
+const gridSizeButton = document.querySelector("button");
+let gridDefaultSize = 16; // default Grid size
+let containerSize = 800;
 
-let gridSize = 16;
-
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-        const squareDivEl = document.createElement("div");
-        squareDivEl.classList.add("box");
-        container.appendChild(squareDivEl);
+const generateGrid = (gridSize=gridDefaultSize) => {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const box = document.createElement("div");
+            const boxSize = `${Math.floor(containerSize / gridSize)}px`;
+            box.style.width = boxSize;
+            box.style.height = boxSize;
+            box.classList.add("box");
+            container.appendChild(box);
+        }
     }
+    const boxesElement = document.querySelectorAll(".box");
+    boxesElement.forEach((box) => {
+        box.addEventListener("mouseover", (e) => {
+            box.style.backgroundColor = "black"
+        })
+    })
 }
+
+generateGrid();
+
+gridSizeButton.addEventListener("click", () => {
+    newSize = parseInt(prompt("Enter the grid size: "));
+    if (newSize > 0) {
+        container.innerHTML = ""; // delete the container content (Grid)
+        console.log(newSize);
+        generateGrid(newSize);
+    }
+    
+})
+
+
+
+
+
+
