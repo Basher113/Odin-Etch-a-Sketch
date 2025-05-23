f10b50c79193cc3a290d827afe8a27cbd42c0fb9
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-23 09:13:28 +0800

    Add random RGB color fill on box hover

--- script.js
+++ script.js
@@ -17,18 +17,25 @@ const generateGrid = (gridSize=gridDefaultSize) => {
     const boxesElement = document.querySelectorAll(".box");
     boxesElement.forEach((box) => {
         box.addEventListener("mouseover", (e) => {
-            box.style.backgroundColor = "black"
+            const randomRGBColor = generateRandomRGBColor()
+            box.style.backgroundColor = randomRGBColor;
         })
     })
 }
 
 generateGrid();
 
+const generateRandomRGBColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
+
 gridSizeButton.addEventListener("click", () => {
     newSize = parseInt(prompt("Enter the grid size: "));
     if (newSize > 0) {
         container.innerHTML = ""; // delete the container content (Grid)
-        console.log(newSize);
         generateGrid(newSize);
     }
     
